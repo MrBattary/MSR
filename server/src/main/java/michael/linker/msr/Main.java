5aae4a56e7b25cb2f2eae4cbdca16843a60fcb5d
Mikhail Linker <work.michaellinker@gmail.com>
2022-12-15 17:11:07 +0300

    A base spring boot server application was generated

---
+++ server/src/main/java/michael/linker/msr/Main.java
@@ -0,0 +1,19 @@
+package michael.linker.msr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Main application entry point
+ */
+@SpringBootApplication
+public class Main {
+	/**
+	 * Main function for app starts
+	 *
+	 * @param args - console arguments
+	 */
+	public static void main(String[] args) {
+		SpringApplication.run(Main.class, args);
+	}
+}
